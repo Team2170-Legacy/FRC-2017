27c90bacd19f58d1867b12f8dd574662cfdc2bc3
nollchr <nollchr@AI191954.utcaus.com>
2017-01-31 13:50:45 -0500

    prototyp pipeline processing code

--- Steamworks/src/Vision/GearTargetPipeline.cpp
+++ Steamworks/src/Vision/GearTargetPipeline.cpp
@@ -1,145 +1,63 @@
 #include "GearTargetPipeline.h"
-/**
-* Initializes a GearTargetPipeline.
-*/
 
 namespace grip {
 
 GearTargetPipeline::GearTargetPipeline() {
 }
 /**
-* Runs an iteration of the Pipeline and updates outputs.
-*
-* Sources need to be set before calling this method. 
-*
+* Runs an iteration of the pipeline and updates outputs.
 */
 void GearTargetPipeline::Process(cv::Mat& source0){
-	//Step Resize_Image0:
-	//input
-	cv::Mat resizeImageInput = source0;
-	double resizeImageWidth = 320.0;  // default Double
-	double resizeImageHeight = 240.0;  // default Double
-	int resizeImageInterpolation = cv::INTER_CUBIC;
-	resizeImage(resizeImageInput, resizeImageWidth, resizeImageHeight, resizeImageInterpolation, this->resizeImageOutput);
-	//Step Blur0:
-	//input
-	cv::Mat blurInput = resizeImageOutput;
-	BlurType blurType = BlurType::GAUSSIAN;
-	double blurRadius = 3.603603603603604;  // default Double
-	blur(blurInput, blurType, blurRadius, this->blurOutput);
 	//Step HSL_Threshold0:
 	//input
-	cv::Mat hslThresholdInput = blurOutput;
-	double hslThresholdHue[] = {55.03597122302158, 107.27272727272727};
-	double hslThresholdSaturation[] = {59.62230215827338, 197.04545454545453};
-	double hslThresholdLuminance[] = {107.77877697841726, 162.70202020202018};
+	cv::Mat hslThresholdInput = source0;
+	double hslThresholdHue[] = {58.45970660364973, 154.5743234426018};
+	double hslThresholdSaturation[] = {103.2308103647633, 254.74095719610364};
+	double hslThresholdLuminance[] = {43.83410567001908, 173.3085241317085};
 	hslThreshold(hslThresholdInput, hslThresholdHue, hslThresholdSaturation, hslThresholdLuminance, this->hslThresholdOutput);
 	//Step Find_Contours0:
 	//input
 	cv::Mat findContoursInput = hslThresholdOutput;
-	bool findContoursExternalOnly = true;  // default Boolean
+	bool findContoursExternalOnly = false;  // default Boolean
 	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
 	//Step Filter_Contours0:
 	//input
 	std::vector<std::vector<cv::Point> > filterContoursContours = findContoursOutput;
-	double filterContoursMinArea = 260.0;  // default Double
-	double filterContoursMinPerimeter = 0;  // default Double
-	double filterContoursMinWidth = 0;  // default Double
-	double filterContoursMaxWidth = 1000;  // default Double
-	double filterContoursMinHeight = 0;  // default Double
-	double filterContoursMaxHeight = 100.0;  // default Double
-	double filterContoursSolidity[] = {0.0, 100.0};
+	double filterContoursMinArea = 0.0;  // default Double
+	double filterContoursMinPerimeter = 0.0;  // default Double
+	double filterContoursMinWidth = 0.0;  // default Double
+	double filterContoursMaxWidth = 10000.0;  // default Double
+	double filterContoursMinHeight = 0.0;  // default Double
+	double filterContoursMaxHeight = 10000.0;  // default Double
+	double filterContoursSolidity[] = {0.0, 99.71018577265724};
 	double filterContoursMaxVertices = 1000000;  // default Double
-	double filterContoursMinVertices = 0;  // default Double
+	double filterContoursMinVertices = 5.0;  // default Double
 	double filterContoursMinRatio = 0;  // default Double
-	double filterContoursMaxRatio = 1000;  // default Double
+	double filterContoursMaxRatio = 100000.0;  // default Double
 	filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, this->filterContoursOutput);
 }
 
 /**
- * This method is a generated setter for source0.
- * @param source the Mat to set
- */
-//void GearTargetPipeline::setsource0(cv::Mat &source0){
-	//source0.copyTo(this->source0);
-//}
-/**
- * This method is a generated getter for the output of a Resize_Image.
- * @return Mat output from Resize_Image.
- */
-cv::Mat* GearTargetPipeline::getresizeImageOutput(){
-	return &(this->resizeImageOutput);
-}
-/**
- * This method is a generated getter for the output of a Blur.
- * @return Mat output from Blur.
- */
-cv::Mat* GearTargetPipeline::getblurOutput(){
-	return &(this->blurOutput);
-}
-/**
  * This method is a generated getter for the output of a HSL_Threshold.
  * @return Mat output from HSL_Threshold.
  */
-cv::Mat* GearTargetPipeline::gethslThresholdOutput(){
+cv::Mat* GearTargetPipeline::GetHslThresholdOutput(){
 	return &(this->hslThresholdOutput);
 }
 /**
  * This method is a generated getter for the output of a Find_Contours.
  * @return ContoursReport output from Find_Contours.
  */
-std::vector<std::vector<cv::Point> >* GearTargetPipeline::getfindContoursOutput(){
+std::vector<std::vector<cv::Point> >* GearTargetPipeline::GetFindContoursOutput(){
 	return &(this->findContoursOutput);
 }
 /**
  * This method is a generated getter for the output of a Filter_Contours.
  * @return ContoursReport output from Filter_Contours.
  */
-std::vector<std::vector<cv::Point> >* GearTargetPipeline::getfilterContoursOutput(){
+std::vector<std::vector<cv::Point> >* GearTargetPipeline::GetFilterContoursOutput(){
 	return &(this->filterContoursOutput);
 }
-	/**
-	 * Scales and image to an exact size.
-	 *
-	 * @param input The image on which to perform the Resize.
-	 * @param width The width of the output in pixels.
-	 * @param height The height of the output in pixels.
-	 * @param interpolation The type of interpolation.
-	 * @param output The image in which to store the output.
-	 */
-	void GearTargetPipeline::resizeImage(cv::Mat &input, double width, double height, int interpolation, cv::Mat &output) {
-		cv::resize(input, output, cv::Size(width, height), 0.0, 0.0, interpolation);
-	}
-
-	/**
-	 * Softens an image using one of several filters.
-	 *
-	 * @param input The image on which to perform the blur.
-	 * @param type The blurType to perform.
-	 * @param doubleRadius The radius for the blur.
-	 * @param output The image in which to store the output.
-	 */
-	void GearTargetPipeline::blur(cv::Mat &input, BlurType &type, double doubleRadius, cv::Mat &output) {
-		int radius = (int)(doubleRadius + 0.5);
-		int kernelSize;
-		switch(type) {
-			case BOX:
-				kernelSize = 2 * radius + 1;
-				cv::blur(input,output,cv::Size(kernelSize, kernelSize));
-				break;
-			case GAUSSIAN:
-				kernelSize = 6 * radius + 1;
-				cv::GaussianBlur(input, output, cv::Size(kernelSize, kernelSize), radius);
-				break;
-			case MEDIAN:
-				kernelSize = 2 * radius + 1;
-				cv::medianBlur(input, output, kernelSize);
-				break;
-			case BILATERAL:
-				cv::bilateralFilter(input, output, -1, radius, radius);
-				break;
-        }
-	}
 	/**
 	 * Segment an image based on hue, saturation, and luminance ranges.
 	 *
@@ -201,11 +119,12 @@ std::vector<std::vector<cv::Point> >* GearTargetPipeline::getfilterContoursOutpu
 			double solid = 100 * area / cv::contourArea(hull);
 			if (solid < solidity[0] || solid > solidity[1]) continue;
 			if (contour.size() < minVertexCount || contour.size() > maxVertexCount)	continue;
-			double ratio = bb.width / bb.height;
+			double ratio = (double) bb.width / (double) bb.height;
 			if (ratio < minRatio || ratio > maxRatio) continue;
 			output.push_back(contour);
 		}
 	}
+
 
 
 } // end grip namespace
