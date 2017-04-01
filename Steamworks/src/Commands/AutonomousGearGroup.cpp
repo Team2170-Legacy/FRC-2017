12f902b5eb2c1bfbb61bce43c0c884aa04cd1671
Mr. Noll <GEH-Magnet School 1@GHSRobotics05>
2017-03-31 22:46:26 -0400

    Added blue boiler moves

--- Steamworks/src/Commands/AutonomousGearGroup.cpp
+++ Steamworks/src/Commands/AutonomousGearGroup.cpp
@@ -17,6 +17,9 @@
 #include "../automoves/RP1_to_RB.h"
 #include "../automoves/RP2_to_RB.h"
 #include "../automoves/RP3_to_RB.h"
+#include "../automoves/BP1_to_BB.h"
+#include "../automoves/BP2_to_BB.h"
+#include "../automoves/BP3_to_BB.h"
 
 AutonomousGearGroup::AutonomousGearGroup(PegLocation loc, bool GotoBoiler) {
 	// TODO Auto-generated constructor stub
@@ -51,7 +54,6 @@ AutonomousGearGroup::AutonomousGearGroup(PegLocation loc, bool GotoBoiler) {
 						&VisionProcessing::e_Gear_x));
 		break;
 	case kLeftPegBlue:
-		AddSequential(new frc::PrintCommand("Left Peg\n"));
 		AddSequential(
 						new AutonomousBlendedMove(&AutoMove_RS3_to_RP3_L,
 								&AutoMove_RS3_to_RP3_R, t_auto_end_RS3_to_RP3,
@@ -74,27 +76,33 @@ AutonomousGearGroup::AutonomousGearGroup(PegLocation loc, bool GotoBoiler) {
 		switch (loc) {
 		case kCenterPegRed:
 			AddSequential(
-					new AutonomousAllianceCmd(
-							new AutonomousMotionProfile(&AutoMove_RP2_to_RB_L,
-									&AutoMove_RP2_to_RB_R),
-							new AutonomousMotionProfile(&AutoMove_RP2_to_RB_L,
-									&AutoMove_RP2_to_RB_R)));
+					new AutonomousMotionProfile(&AutoMove_RP2_to_RB_L,
+							&AutoMove_RP2_to_RB_R));
 			break;
 		case kLeftPegRed:
 			AddSequential(
-					new AutonomousAllianceCmd(
-							new AutonomousMotionProfile(&AutoMove_RP1_to_RB_L,
-									&AutoMove_RP1_to_RB_R),
-							new AutonomousMotionProfile(&AutoMove_RP1_to_RB_L,
-									&AutoMove_RP3_to_RB_R)));
+					new AutonomousMotionProfile(&AutoMove_RP1_to_RB_L,
+							&AutoMove_RP1_to_RB_R));
 			break;
 		case kRightPegRed:
 			AddSequential(
-					new AutonomousAllianceCmd(
-							new AutonomousMotionProfile(&AutoMove_RP3_to_RB_L,
-									&AutoMove_RP3_to_RB_R),
-							new AutonomousMotionProfile(&AutoMove_RP3_to_RB_L,
-									&AutoMove_RS1_to_RP1_R)));
+					new AutonomousMotionProfile(&AutoMove_RP3_to_RB_L,
+							&AutoMove_RP3_to_RB_R));
+			break;
+		case kCenterPegBlue:
+			AddSequential(
+					new AutonomousMotionProfile(&AutoMove_BP2_to_BB_L,
+							&AutoMove_BP2_to_BB_R));
+			break;
+		case kRightPegBlue:
+			AddSequential(
+					new AutonomousMotionProfile(&AutoMove_BP1_to_BB_L,
+							&AutoMove_RP1_to_RB_R));
+			break;
+		case kLeftPegBlue:
+			AddSequential(
+					new AutonomousMotionProfile(&AutoMove_RP3_to_RB_L,
+							&AutoMove_RP3_to_RB_R));
 			break;
 		default:
 			break;
